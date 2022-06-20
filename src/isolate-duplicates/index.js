633612c40328d2610c5d9df8ceeabc88b397f8d1
yemi <olowofela.olanrewaju@decagon.dev>
2022-06-20 12:12:24 +0100

    update

--- src/isolate-duplicates/index.js
+++ src/isolate-duplicates/index.js
@@ -1,3 +1,19 @@
-function isolateDuplicates(text) {}
+// function isolateDuplicates(text) {}
+// module.exports = isolateDuplicates;
 
-module.exports = isolateDuplicates;
+const isolateDuplicates = (str) => {
+  const arr = str.split('');
+  const duplicateChars = [];
+  const sortedArr = [arr].sort();
+
+  sortedArr.forEach ((elem, index) => {
+    if (sortedArr[index + 1] === elem) {
+      duplicateChars.push(elem);
+    }
+  })
+return duplicateChars;
+}
+
+//let duplicateChars = isolateDuplicates ('a,b,c,d,e,b,c,a');
+
+console.log (isolateDuplicates ('abcacdbc'));
