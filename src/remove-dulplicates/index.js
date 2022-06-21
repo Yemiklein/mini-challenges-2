a0beab66658f9b84ec86684be8e27338f85dbab7
yemi <olowofela.olanrewaju@decagon.dev>
2022-06-21 13:54:37 +0100

    new update

--- src/remove-dulplicates/index.js
+++ src/remove-dulplicates/index.js
@@ -1,3 +1,29 @@
-function removeDuplicates(obj) {}
+function removeDuplicates(obj) {
+
+  for (let objKey in obj){
+
+   obj[objKey] = [...new Set(obj[objKey])];
+  }
+
+  const tempObj = obj;
+//loop1 through each element
+  for (let objKey in obj) {
+    obj[objKey].forEach(element => {
+//loop 2
+      for(let objKey2 in tempObj) {
+        tempObj[objKey2].forEach(element2 => {
+
+          if(element === element2 && objKey !=
+             objKey2) {
+            tempObj[objKey] = tempObj[objKey].filter(
+              (value) => value !== element)
+          }
+        });
+      };
+    });
+  }
+  return tempObj
+}
 
 module.exports = removeDuplicates;
+
